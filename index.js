4e3f3c5c2a1cdbd1fd12fcb196c69361bf61b60b
chris.lee <neoxrock@gmail.com>
2021-11-15 10:48:19 +1100

    actions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Parcel");
+// select DOM
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// action names
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+// action creation functions
+const toggleSwitch = () => ({
+  type: TOGGLE_SWITCH,
+});
+const increase = (difference) => ({
+  type: INCREASE,
+  difference,
+});
+const decrease = () => ({
+  type: DECREASE,
+});
